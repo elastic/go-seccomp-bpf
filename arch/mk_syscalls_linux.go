26551f68bd00dc6dce1eccc7e064da678406bf56
Alvar Penning <post@0x21.biz>
2022-11-24 22:13:50 +0100

    Update syscalls for Linux v6.0

--- arch/mk_syscalls_linux.go
+++ arch/mk_syscalls_linux.go
@@ -394,7 +394,7 @@ var (
 
 func init() {
 	flag.StringVar(&outputFile, "out", "zsyscalls.go", "output file")
-	flag.StringVar(&linuxVersion, "version", "v5.14", "linux version (git tag)")
+	flag.StringVar(&linuxVersion, "version", "v6.0", "linux version (git tag)")
 }
 
 func main() {
