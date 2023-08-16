9456ee93f8b5cc1fe3bec310699be543557062c8
Svetlana <5437289+leo-ri@users.noreply.github.com>
2023-08-16 17:02:56 +0200

    cleanup jenkins files

--- .ci/scripts/gofumpt.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-go install mvdan.cc/gofumpt@latest
-
-if [ "$(gofumpt -l $(find . -name "*.go") | wc -l)" -gt 0 ]; then
-    echo "Run gofumpt on the code"
-    exit 1
-fi
