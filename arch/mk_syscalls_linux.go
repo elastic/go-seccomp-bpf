2d21a68c22f249d38c596f1df5fe6d35b5c0193d
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2025-03-25 08:13:38 -0300

    chore: update mk_syscalls_linux to v6.14 (#43)
    No new syscalls on this kernel release, just updating the kernel version.

--- arch/mk_syscalls_linux.go
+++ arch/mk_syscalls_linux.go
@@ -394,7 +394,7 @@ var (
 
 func init() {
 	flag.StringVar(&outputFile, "out", "zsyscalls.go", "output file")
-	flag.StringVar(&linuxVersion, "version", "v6.13", "linux version (git tag)")
+	flag.StringVar(&linuxVersion, "version", "v6.14", "linux version (git tag)")
 }
 
 func main() {
